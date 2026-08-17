eb2f80c5856d47b4afad88cb0a93f00daadb70f2
arieba4 <arieba4@gmail.com>
2026-08-17 12:09:06 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE="eurostock-v2-1";
+const ASSETS=["./","./index.html","./manifest.webmanifest"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET") return;
+  const u=new URL(e.request.url);
+  if(u.origin!==location.origin) return;
+  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));
+});
